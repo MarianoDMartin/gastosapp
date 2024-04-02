45947200e269ca4ecf4d980f0090ad1d51a6e8e0
mariano <mariano@whereto.com>
2024-04-01 21:14:04 -0300

    modify greetings to test be ci

--- BE/controllers/greetings.ts
+++ BE/controllers/greetings.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 async function hello(req: Request, res: Response) {
-  return res.send('Hello World!')
+  return res.send('Hello World')
 }
 
 export default {
